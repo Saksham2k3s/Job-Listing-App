0eeaeba10f9ebb66fe571870835dd1209565663e
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-07-17 15:37:52 +0530

    Removed extra code

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,5 @@ app.use(express.json());
 app.use('/api/jobs', jobRouter)
 
 // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
